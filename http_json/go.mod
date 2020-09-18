7f70de78ed0db7ebba38922874a73d29ba79e022
Marcelo Aguero <marceloaguero@gmail.com>
2020-09-18 12:07:02 -0300

    Add http_json helper

---
+++ http_json/go.mod
@@ -0,0 +1,9 @@
+module github.com/BetuelSA/go-helpers/http_json
+
+go 1.15
+
+require (
+	github.com/BetuelSA/go-helpers v0.0.0-20200817213659-0b5198570e56
+	github.com/google/jsonapi v0.0.0-20200825183604-3e3da1210d0c
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ http_json/http_json.go
@@ -0,0 +1,132 @@
+package http_json
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/BetuelSA/go-helpers/errors"
+	"github.com/google/jsonapi"
+)
+
+const (
+	headerContentType = "Content-Type"
+)
+
+func writeJSON(w http.ResponseWriter, code int, data interface{}) {
+	response, _ := json.Marshal(data)
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func writeErrJSON(w http.ResponseWriter, err error) {
+	var code int
+	errorType := errors.GetType(err)
+
+	switch errorType {
+	case errors.BadRequest:
+		code = http.StatusBadRequest
+	case errors.Unauthorized:
+		code = http.StatusUnauthorized
+	case errors.Forbidden:
+		code = http.StatusForbidden
+	case errors.NotFound:
+		code = http.StatusNotFound
+	case errors.MethodNotAllowed:
+		code = http.StatusMethodNotAllowed
+	case errors.PreconditionFailed:
+		code = http.StatusPreconditionFailed
+	case errors.UnsupportedMediaType:
+		code = http.StatusUnsupportedMediaType
+	case errors.InternalServerError:
+		code = http.StatusInternalServerError
+	case errors.NotImplemented:
+		code = http.StatusNotImplemented
+	case errors.ServiceUnavailable:
+		code = http.StatusServiceUnavailable
+	default:
+		code = http.StatusInternalServerError
+	}
+
+	if errorType == errors.NoType {
+		log.Printf(err.Error())
+	}
+
+	var errorMap = map[string]interface{}{
+		"error":   code,
+		"message": errors.GetErrorMessage(err),
+		"detail":  errors.GetErrorDetail(err),
+	}
+
+	errorContext := errors.GetErrorContext(err)
+	if errorContext != nil {
+		errorMap["context"] = errorContext
+	}
+
+	writeJSON(w, code, errorMap)
+}
+
+func writeJSONAPI(w http.ResponseWriter, code int, data interface{}) {
+	jsonapiRuntime := jsonapi.NewRuntime()
+
+	w.Header().Set(headerContentType, jsonapi.MediaType)
+	w.WriteHeader(code)
+
+	err := jsonapiRuntime.MarshalPayload(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func writeErrJSONAPI(w http.ResponseWriter, err error) {
+	var code int
+	errorType := errors.GetType(err)
+
+	switch errorType {
+	case errors.BadRequest:
+		code = http.StatusBadRequest
+	case errors.Unauthorized:
+		code = http.StatusUnauthorized
+	case errors.Forbidden:
+		code = http.StatusForbidden
+	case errors.NotFound:
+		code = http.StatusNotFound
+	case errors.MethodNotAllowed:
+		code = http.StatusMethodNotAllowed
+	case errors.PreconditionFailed:
+		code = http.StatusPreconditionFailed
+	case errors.UnsupportedMediaType:
+		code = http.StatusUnsupportedMediaType
+	case errors.InternalServerError:
+		code = http.StatusInternalServerError
+	case errors.NotImplemented:
+		code = http.StatusNotImplemented
+	case errors.ServiceUnavailable:
+		code = http.StatusServiceUnavailable
+	default:
+		code = http.StatusInternalServerError
+	}
+
+	if errorType == errors.NoType {
+		log.Printf(err.Error())
+	}
+
+	var errorObject = jsonapi.ErrorObject{
+		Status: strconv.Itoa(code),
+		Title:  errors.GetErrorMessage(err),
+		Detail: errors.GetErrorDetail(err),
+	}
+
+	errorObjects := []*jsonapi.ErrorObject{&errorObject}
+
+	w.Header().Set(headerContentType, jsonapi.MediaType)
+	w.WriteHeader(code)
+
+	err = jsonapi.MarshalErrors(w, errorObjects)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
