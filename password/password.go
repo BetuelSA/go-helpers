0e84e2725b3627d896e6192c7cf1c553e2972d29
Marcelo Aguero <marceloaguero@gmail.com>
2020-10-05 21:12:02 -0300

    Add password helper

---
+++ password/password.go
@@ -0,0 +1,29 @@
+package password
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+type password struct{}
+
+// PasswordSvc --
+type PasswordSvc interface {
+	Hash(password string) (string, error)
+	CheckPassword(password, hash string) error
+}
+
+// NewPasswordSvc creates a new fake password
+func NewPasswordSvc() PasswordSvc {
+	return &password{}
+}
+
+// Hash returns the hash for a password
+func (p *password) Hash(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hash), err
+}
+
+// CheckPassword compares a password and its hash, validating it
+func (p *password) CheckPassword(password, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
